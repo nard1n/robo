47914c348d72bec620c7d9dff6cd3ec3d6e14c75
Nardin Lachowski <nardin@live.com>
2021-03-20 21:09:46 -0400

    replacing the Card component to the new CardList component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <div>
-    <Card />
-    <Card />
-    <Card />
-  </div>,
+  <CardList robots={robots} />,
   document.getElementById('root')
 );
 
