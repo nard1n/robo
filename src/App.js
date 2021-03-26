89a7e29a6ddb1942b14b2de770c77fecb6d5a7ba
Nardin Lachowski <nardin@live.com>
2021-03-26 10:53:31 -0400

    adding scroll js with scrollable functionality to page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import Scroll from './Scroll';
 import './App.css';
+import { robots } from './robots';
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users}));
     }
 
     onNewSearch = (event) => {
@@ -20,17 +27,22 @@ class App extends Component {
     
     render() {
         const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase());
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
+        if (robots.length === 0 ){
+            return <h1>Loading</h1>
+        } else{
         return (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onNewSearch} />
-                <CardList robots={filteredRobots} />
+                <Scroll>
+                    <CardList robots={filteredRobots} />
+                </Scroll>
             </div>
         );
     }
-    
+    }    
 }
 
 export default App;
