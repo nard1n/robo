141afb4c866b1d4c58199ce5e6b1ff01c522a106
Nardin Lachowski <nardin@live.com>
2021-03-26 11:17:08 -0400

    optimizing if else statement

--- src/containers/App.js
+++ src/containers/App.js
@@ -29,10 +29,9 @@ class App extends Component {
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (!robots.length){
-            return <h1>Loading</h1>
-        } else {
-        return (
+        return !robots.length ?
+            <h1>Loading</h1> :
+            (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
                 <SearchBox searchChange={this.onNewSearch} />
@@ -41,8 +40,7 @@ class App extends Component {
                 </Scroll>
             </div>
         );
-    }
-    }    
+    }   
 }
 
 export default App;
