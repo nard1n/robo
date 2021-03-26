724d49d2258d07e38eec0feda139528ca4b8932f
Nardin Lachowski <nardin@live.com>
2021-03-26 12:41:10 -0400

    replaced react state class with functional component using usestate hook

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: ''
-        }
+function App() {
+
+   const [robots, setRobots] = useState([])
+   const [searchfield, setSearchfield] = useState('')
+
+    // componentDidMount() {
+    //     fetch('http://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ robots: users}));
+    //  }
+
+    const onNewSearch = (event) => {
+        setSearchfield(event.target.value)
     }
 
-    componentDidMount() {
-        fetch('http://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(users => this.setState({ robots: users}));
-    }
-
-    onNewSearch = (event) => {
-        this.setState({ searchfield: event.target.value })
-    }
-    
-    render() {
-        const { robots, searchfield } = this.state;
         const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
@@ -34,13 +28,12 @@ class App extends Component {
             (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onNewSearch} />
+                <SearchBox searchChange={onNewSearch} />
                 <Scroll>
                     <CardList robots={filteredRobots} />
                 </Scroll>
             </div>
         );
     }   
-}
 
 export default App;
